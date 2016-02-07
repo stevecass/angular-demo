737077970f03f3098e6153f543a8417fdc74bdb8
Steven Cassidy <steven.cassidy@gmail.com>
2016-02-07 11:06:46 -0500

    Move controller to its own file

---
+++ client/js/controller.js
@@ -0,0 +1,8 @@
+angular.module('contacts').controller('ContactsController', ['$scope', function($scope) {
+  $scope.contacts = CANNED_PEOPLE;
+
+  $scope.showContact = function(contact) {
+    $scope.currentContact = contact;
+  }
+
+}]);
