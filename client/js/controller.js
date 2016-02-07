e194768f075001f4a64080cb27608e1d58d7c97c
Steven Cassidy <steven.cassidy@gmail.com>
2016-02-07 11:15:17 -0500

    Switch controller to use factory

--- client/js/controller.js
+++ client/js/controller.js
@@ -1,5 +1,8 @@
-angular.module('contacts').controller('ContactsController', ['$scope', function($scope) {
-  $scope.contacts = CANNED_PEOPLE;
+angular.module('contacts').controller('ContactsController', ['Person', '$scope', function(Person, $scope) {
+
+  Person.all().then(function(response){
+    $scope.contacts = response;
+  });
 
   $scope.showContact = function(contact) {
     $scope.currentContact = contact;
